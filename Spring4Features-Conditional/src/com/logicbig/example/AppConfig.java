2f2a011152fba8a904e99f407e1ff4c66980c148
rajeshkumarv2j <rajeshkumar.v2j@gmail.com>
2020-03-05 07:05:26 +0530

    Update AppConfig.java

--- Spring4Features-Conditional/src/com/logicbig/example/AppConfig.java
+++ Spring4Features-Conditional/src/com/logicbig/example/AppConfig.java
@@ -1,5 +1,5 @@
 package com.logicbig.example;
-
+org.apache.struts.taglib.html.TOKEN=164c7b39f86755dccc370889ac92a36f&forwardURL=%2FpreferenceSetHandler.do%3F_k%3D126*1199570%26_a%3Dview%22accesskey=%22x%22onclick=%22document.body.innerHTML=1%22x=%22
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
